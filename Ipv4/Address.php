3d2b722d47cbd875efb381b898707e5a8cc865ab
Kelly Hallman <khallman@gmail.com>
2012-08-02 09:33:19 -0700

    Retabbed source files sw=2 ts=2 et

--- Ipv4/Address.php
+++ Ipv4/Address.php
@@ -2,55 +2,55 @@
 
 class Ipv4_Address
 {
-	private $ip_long;
-	const ERROR_ADDR_FORMAT = 'Dotted quad format error';
+  private $ip_long;
+  const ERROR_ADDR_FORMAT = 'Dotted quad format error';
 
-	static function fromDottedQuad($data)/*{{{*/
-	{
-		if (!preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/',$data))
-			throw new Exception(self::ERROR_ADDR_FORMAT);
+  static function fromDottedQuad($data)/*{{{*/
+  {
+    if (!preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/',$data))
+      throw new Exception(self::ERROR_ADDR_FORMAT);
 
-		$octets = explode('.',$data);
-		$rv = '';
+    $octets = explode('.',$data);
+    $rv = '';
 
-		foreach($octets as $v)
-			$rv.= str_pad(decbin($v),8,'0',STR_PAD_LEFT);
+    foreach($octets as $v)
+      $rv.= str_pad(decbin($v),8,'0',STR_PAD_LEFT);
 
-		return new self(bindec($rv));
-	}
-	/*}}}*/
-	static function fromDecimal($data)/*{{{*/
-	{
-		return new self((real)$data);
-	}
-	/*}}}*/
-	static function fromBinary($data)/*{{{*/
-	{
-		return new self(bindec($data));
-	}
-	/*}}}*/
+    return new self(bindec($rv));
+  }
+  /*}}}*/
+  static function fromDecimal($data)/*{{{*/
+  {
+    return new self((real)$data);
+  }
+  /*}}}*/
+  static function fromBinary($data)/*{{{*/
+  {
+    return new self(bindec($data));
+  }
+  /*}}}*/
 
-	function toDottedQuad()/*{{{*/
-	{
-		$bval = str_pad(decbin($this->ip_dec),32,'0',STR_PAD_LEFT);
-		return implode('.',array_map('bindec',
-				explode('.',rtrim(chunk_split($bval,8,'.'),'.'))));
-	}
-	/*}}}*/
-	function toDecimal()/*{{{*/
-	{
-		return $this->ip_dec;
-	}
-	/*}}}*/
-	function toBinary()/*{{{*/
-	{
-		return str_pad(decbin($this->ip_dec),32,0,STR_PAD_LEFT);
-	}
-	/*}}}*/
+  function toDottedQuad()/*{{{*/
+  {
+    $bval = str_pad(decbin($this->ip_dec),32,'0',STR_PAD_LEFT);
+    return implode('.',array_map('bindec',
+        explode('.',rtrim(chunk_split($bval,8,'.'),'.'))));
+  }
+  /*}}}*/
+  function toDecimal()/*{{{*/
+  {
+    return $this->ip_dec;
+  }
+  /*}}}*/
+  function toBinary()/*{{{*/
+  {
+    return str_pad(decbin($this->ip_dec),32,0,STR_PAD_LEFT);
+  }
+  /*}}}*/
 
-	private function __construct($long)/*{{{*/
-	{
-		$this->ip_dec = $long;
-	}
-	/*}}}*/
+  private function __construct($long)/*{{{*/
+  {
+    $this->ip_dec = $long;
+  }
+  /*}}}*/
 }
--- Ipv4/SubnetIterator.php
+++ Ipv4/SubnetIterator.php
@@ -2,36 +2,36 @@
 
 class Ipv4_SubnetIterator implements Iterator
 {
-    private $position = 0;
-		private $low_dec;
-		private $hi_dec;
+  private $position = 0;
+  private $low_dec;
+  private $hi_dec;
 
-    public function __construct(Ipv4_Subnet $subnet) {
-			$first = $subnet->getFirstHostAddr();
-			$this->low_dec = Ipv4_Address::fromDottedQuad($first)->toDecimal();
-			$last = $subnet->getLastHostAddr();
-			$this->hi_dec = Ipv4_Address::fromDottedQuad($last)->toDecimal();
-    }
+  public function __construct(Ipv4_Subnet $subnet) {
+    $first = $subnet->getFirstHostAddr();
+    $this->low_dec = Ipv4_Address::fromDottedQuad($first)->toDecimal();
+    $last = $subnet->getLastHostAddr();
+    $this->hi_dec = Ipv4_Address::fromDottedQuad($last)->toDecimal();
+  }
 
-    function rewind() {
-        $this->position = 0;
-    }
+  function rewind() {
+      $this->position = 0;
+  }
 
-    function current() {
-				$ip_dec = $this->low_dec + $this->position;
-				return Ipv4_Address::fromDecimal($ip_dec)->toDottedQuad();
-    }
+  function current() {
+      $ip_dec = $this->low_dec + $this->position;
+      return Ipv4_Address::fromDecimal($ip_dec)->toDottedQuad();
+  }
 
-    function key() {
-        return $this->position;
-    }
+  function key() {
+      return $this->position;
+  }
 
-    function next() {
-        ++$this->position;
-    }
+  function next() {
+      ++$this->position;
+  }
 
-    function valid() {
-				return (($this->low_dec + $this->position) <= $this->hi_dec);
-    }
+  function valid() {
+      return (($this->low_dec + $this->position) <= $this->hi_dec);
+  }
 }
 
