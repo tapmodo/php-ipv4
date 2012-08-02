283c27d664f8c568139fb0e48d69b8fd8cc144b7
Kelly Hallman <khallman@gmail.com>
2012-08-02 10:22:14 -0700

    Added phpdoc docblocks to Subnet.php

--- Ipv4/Subnet.php
+++ Ipv4/Subnet.php
@@ -8,11 +8,28 @@ class Ipv4_Subnet
   const ERROR_CIDR_FORMAT = 'Invalid CIDR format';
   const ERROR_SUBNET_FORMAT = 'Invalid Subnet format';
 
+  /**
+   * __construct
+   * Public constructor
+   *
+   * @param mixed $n Network
+   * @param mixed $s Subnet
+   * @access public
+   * @return void
+   */
   public function __construct($n=null,$s=null) {
     if (is_string($n) and !$s) $this->fromString($n);
       elseif ($n and $s) $this->setNetwork($n)->setNetmask($s);
   }
 
+  /**
+   * fromString
+   * Parse subnet string
+   *
+   * @param string $data
+   * @access public
+   * @return void
+   */
   public function fromString($data) {
     if (!preg_match('!^([0-9]{1,3}\.){3}[0-9]{1,3}(( ([0-9]{1,3}\.){3}[0-9]{1,3})|(/[0-9]{1,2}))$!',$data))
       throw new Exception(self::ERROR_NETWORK_FORMAT);
@@ -32,11 +49,27 @@ class Ipv4_Subnet
     }
   }
 
+  /**
+   * setNetwork
+   * Sets the network on the object, from dotted-quad notation
+   *
+   * @param string $data
+   * @access public
+   * @return self
+   */
   public function setNetwork($data) {
     $this->nw = Ipv4_Address::fromDottedQuad($data)->toDecimal();
     return $this;
   }
 
+  /**
+   * setNetmask
+   * Sets the netmask on the object, from dotted-quad notation
+   *
+   * @param string $data
+   * @access public
+   * @return self
+   */
   public function setNetmask($data) {
     $data = Ipv4_Address::fromDottedQuad($data);
 
@@ -47,46 +80,109 @@ class Ipv4_Subnet
     return $this;
   }
 
+  /**
+   * getNetmask
+   * Returns the netmask as dotted-quad string
+   *
+   * @access public
+   * @return string
+   */
   public function getNetmask() {
     return Ipv4_Address::fromDecimal($this->sn)->toDottedQuad();
   }
-  
+
+  /**
+   * getNetmaskCidr
+   * Returns the CIDR value representing the netmask
+   *
+   * @access public
+   * @return int
+   */
   public function getNetmaskCidr() {
     return strlen(rtrim(decbin($this->sn),'0'));
   }
-  
+
+  /**
+   * getNetwork
+   * Returns the network address in dotted-quad notation
+   *
+   * @access public
+   * @return string
+   */
   public function getNetwork() {
     $nw_bin = Ipv4_Address::fromDecimal($this->nw)->toBinary();
     $nw_bin = (str_pad(substr($nw_bin,0,$this->getNetmaskCidr()),32,0));
     return Ipv4_Address::fromBinary($nw_bin)->toDottedQuad();
   }
-  
+
+  /**
+   * getFirstHostAddr
+   * Returns the first address of this network
+   *
+   * @access public
+   * @return string
+   */
   public function getFirstHostAddr() {
     $bin_net = Ipv4_Address::fromDottedQuad($this->getNetwork())->toBinary();
     $bin_first = (str_pad(substr($bin_net,0,31),32,1));
     return Ipv4_Address::fromBinary($bin_first)->toDottedQuad();
   }
-  
+
+  /**
+   * getLastHostAddr
+   * Returns last host of this network
+   *
+   * @access public
+   * @return string
+   */
   public function getLastHostAddr() {
     $bin_bcast = Ipv4_Address::fromDottedQuad($this->getBroadcastAddr())->toBinary();
     $bin_last = (str_pad(substr($bin_bcast,0,31),32,0));
     return Ipv4_Address::fromBinary($bin_last)->toDottedQuad();
   }
-  
+
+  /**
+   * getBroadcastAddr
+   * Returns the broadcast address for this network
+   *
+   * @access public
+   * @return string
+   */
   public function getBroadcastAddr() {
     $bin_host = Ipv4_Address::fromDecimal($this->nw)->toBinary();
     $bin_bcast = str_pad(substr($bin_host,0,$this->getNetmaskCidr()),32,1);
     return Ipv4_Address::fromBinary($bin_bcast)->toDottedQuad();
   }
-  
+
+  /**
+   * getTotalHosts
+   * Returns a count of the total number of hosts on this network
+   *
+   * @access public
+   * @return int
+   */
   public function getTotalHosts() {
     return (bindec(str_pad("",(32-$this->getNetmaskCidr()),1)) - 1);
   }
-  
+
+  /**
+   * getIterator
+   * Returns an iterator for addresses in this subnet
+   *
+   * @access public
+   * @return Ipv4_SubnetIterator
+   */
   public function getIterator() {
     return new Ipv4_SubnetIterator($this);
   }
-  
+
+  /**
+   * __toString
+   * Magic method prints subnet in IP/cidr format
+   *
+   * @access public
+   * @return string
+   */
   public function __toString() {
     return sprintf(
       '%s/%s',
